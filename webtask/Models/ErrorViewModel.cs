b5fd37f337aa72024b085ea3e929c0cd3f629d32
dell <dell@DESKTOP-RRR2725>
2022-06-28 12:24:46 -0700

    Add project files.

---
+++ webtask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webtask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
